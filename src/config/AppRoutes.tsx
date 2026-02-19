1b41cb71369aedc775222547acf60ed7c540084a
“pitapettgit config --global user.email “paldrin368@gmail.com <paldrin368@gmail.com>
2026-02-20 03:06:41 +0700

    first-commit

---
+++ src/config/AppRoutes.tsx
@@ -0,0 +1,8 @@
+import { lazy } from "react"
+import { Routes, Route } from "react-router"
+
+export const AppRoutes = () => {
+    return <Routes>
+       
+    </Routes>
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import { AppRoutes } from './config/AppRoutes.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </StrictMode>,
+)
